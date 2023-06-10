325b106080fa6e159daa89d094550b19871e034c
ohsg0226 <ohsg0226@naver.com>
2023-06-10 17:01:39 +0900

    프로젝트 초기 세팅

---
+++ conference.ios/conference.ios/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  conference.ios
+//
+//  Created by 오승기 on 2023/06/10.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
