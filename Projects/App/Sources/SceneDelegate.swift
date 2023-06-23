4b5f2b4c4e7e8f591d18037c371aeaed464da57b
Goban <gobanest@gmail.com>
2023-06-23 15:45:46 +0900

    [setting] change interface swiftui to uikit

---
+++ Projects/App/Sources/SceneDelegate.swift
@@ -0,0 +1,31 @@
+import Swinject
+import UIKit
+import Home
+import Domain
+import Data
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+    func scene(
+        _ scene: UIScene,
+        willConnectTo session: UISceneSession,
+        options connectionOptions: UIScene.ConnectionOptions
+    ) {
+        guard let scene = (scene as? UIWindowScene) else { return }
+        let navigationController = HomeViewController()
+        window = .init(windowScene: scene)
+        window?.rootViewController = navigationController
+        window?.makeKeyAndVisible()
+    }
+
+    func sceneDidDisconnect(_ scene: UIScene) {}
+
+    func sceneDidBecomeActive(_ scene: UIScene) {}
+
+    func sceneWillResignActive(_ scene: UIScene) {}
+
+    func sceneWillEnterForeground(_ scene: UIScene) {}
+
+    func sceneDidEnterBackground(_ scene: UIScene) {}
+}
---
+++ Projects/Home/Sources/HomeViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  Home
+//
+//  Created by Lee Myeonghwan on 2023/06/23.
+//  Copyright © 2023 org.soma.ios. All rights reserved.
+//
+
+import UIKit
+
+public class HomeViewController: UIViewController {
+
+    public override func viewDidLoad() {
+        super.viewDidLoad()
+    
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
