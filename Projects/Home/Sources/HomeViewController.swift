49e4398b85b238ae0c9055bbc0785ffed2c8c9fc
ace kim <ace.jckim@gmail.com>
2023-06-25 16:11:29 +0900

    기초 View 세팅 완료

--- Projects/Home/Sources/HomeViewController.swift
+++ Projects/Home/Sources/HomeViewController.swift
@@ -9,22 +9,35 @@
 import UIKit
 
 public class HomeViewController: UIViewController {
-
+    let viewModel: HomeViewModel
+    
+    public init() {
+        self.viewModel = HomeViewModel()
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    @available(*, unavailable, message: "remove required init")
+    public required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     public override func viewDidLoad() {
         super.viewDidLoad()
-    
+        
+        view.backgroundColor = .white
+        
         // Do any additional setup after loading the view.
     }
     
-
+    
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
---
+++ Projects/Home/Sources/HomeViewModel.swift
@@ -0,0 +1,24 @@
+//
+//  HomeViewModel.swift
+//  Home
+//
+//  Created by Ace Kim on 2023/06/25.
+//  Copyright © 2023 org.soma.ios. All rights reserved.
+//
+
+import Foundation
+
+protocol HomeViewModelProtocol {
+    func getWeather()
+    func getTodayForecastWeather()
+}
+
+class HomeViewModel : HomeViewModelProtocol {
+    func getWeather() {
+        
+    }
+    
+    func getTodayForecastWeather() {
+        
+    }
+}
